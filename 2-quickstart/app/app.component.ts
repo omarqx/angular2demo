57fd6b603deeb5e8876f06966ab68b98ed076b85
Omar Qaddoumi <omarqaddoumi@gmail.com>
2016-11-06 22:25:00 -0800

    hello world second

--- 2-quickstart/app/app.component.ts
+++ 2-quickstart/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello Angular!</h1>`
+    template: `<h1>Hello {{name}}!</h1>
+               <input  [(ngModel)]="name" />`,
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent { 
-    
+export class AppComponent {
+
 }
