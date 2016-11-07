dff88d184aab3734b8d81a24bbfa80d73465a08c
Omar Qaddoumi <omarqaddoumi@gmail.com>
2016-11-07 00:54:31 -0800

    fill in data

--- cliDemo/src/app/load-posts.directive.ts
+++ cliDemo/src/app/load-posts.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer } from '@angular/core';
+import { PostsService } from './posts.service';
 
 @Directive({
-  selector: '[appLoadPosts]'
+  selector: '[appLoadPosts]',
+  providers: [
+    PostsService
+  ]
 })
 export class LoadPostsDirective {
-
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer,private postsService:PostsService) { }
+  @HostListener('mousedown',['$event']) onMouseDown(e:MouseEvent) {
+    this.postsService.getPostsByUserId(this.el.nativeElement.attributes["data-id"].value)
+    .then((x)=>this.setPosts(x.length));
+  }
+  private setPosts(number) {
+    let h = document.createElement("span")                // Create a <h1> element
+    let t = document.createTextNode(number);     // Create a text node
+    h.appendChild(t);
+    this.el.nativeElement.appendChild(h);
+  }
 
 }
--- cliDemo/src/app/posts.service.ts
+++ cliDemo/src/app/posts.service.ts
@@ -1,8 +1,39 @@
+import {Post} from './post';
 import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+
+// Import RxJs required methods
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class PostsService {
+  private postsUrl = 'http://localhost:3000/posts';
+  constructor(private http:Http) { }
 
-  constructor() { }
+  getPostsByUserId(UserId:number): Promise<Post[]> {
+    let searchQ = `${this.postsUrl}?userId=${UserId}`;
+    return this.http
+      .get(searchQ)
+      .toPromise()
+      .then(response => {
+        return response.json() as Post[];
+      })
+      .catch(this.handleError);
+  }
 
+  getPostsByUserId$(UserId:number) : Observable<Post[]> {
+         return this.http.get(`${this.postsUrl}?userId=${UserId}`)
+                         .map((res:Response) => res.json())
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+
+     }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
 }
